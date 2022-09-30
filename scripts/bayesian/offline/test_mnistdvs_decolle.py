2814437ef569ab0786209ed21cc0263ed1552ce1
Nicolas Skatchkovsky <nicolas@nmslap007198.client.kcl.ac.uk>
2022-09-30 13:40:05 +0100

    fix optimizer state loader

--- scripts/bayesian/offline/test_mnistdvs_decolle.py
+++ scripts/bayesian/offline/test_mnistdvs_decolle.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     print([torch.mean(latent_tensor) for latent_tensor in loaded_dict['param_groups'][0]['latent']])
 
     optimizer.load_state_dict(torch.load(os.path.join(results_path, r'optim_state_dict.pt')))
-    print([torch.mean(latent_tensor) for latent_tensor in loaded_dict['param_groups'][0]['latent']])
+    # print([torch.mean(latent_tensor) for latent_tensor in loaded_dict['param_groups'][0]['latent']])
     print([torch.mean(latent_tensor) for latent_tensor in optimizer.param_groups[0]['latent']])
 
     _, test_dl = create_dataloader(dataset_path,
