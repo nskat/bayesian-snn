932e9beb8cb4447f76c40a1545b8c03f0cf8a0e6
Nicolas Skatchkovsky <nicolas@nmslap007198.client.kcl.ac.uk>
2022-09-30 12:24:25 +0100

    fix optimizer state loader

--- optim/optimizer.py
+++ optim/optimizer.py
@@ -188,18 +188,17 @@ class BayesOptimizer(torch.optim.Optimizer):
                 state[k] = v
 
         # Update parameter groups, setting their params and Bayesian params value
-        def cast_group(group, new_group):
-            for k, v in new_group.items():
-                if (k != 'params') and isinstance(new_group[k], list):
-                    for (new_params, old_params) in zip(new_group[k], group[k]):
-                        if isinstance(new_params[0], torch.Tensor):
-                            for new_param, old_param in zip(new_params, old_params):
-                                new_param = new_param.to(old_param.device)
-            return new_group
+        def cast_params(params, new_params):
+            for i in range(len(new_params)):
+                if isinstance(new_params[i], torch.Tensor):
+                    new_params[i] = new_params[i].to(params[i].device)
+            return new_params
 
         def update_group(group, new_group):
             new_group['params'] = group['params']
-            new_group = cast_group(group, new_group)
+            for k, v in new_group.items():
+                if (k != 'params') and isinstance(new_group[k], list):
+                    new_group[k] = cast_params(group[k], new_group[k])
             return new_group
         param_groups = [
             update_group(g, ng) for g, ng in zip(groups, saved_groups)]
